3c6475729534e424022a1b27d15cd167b8d9d082
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-06 18:53:46 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import Title from "./components/Title";
+import "./App.css";
 
-function App() {
+const initialData = [
+  {
+    _id: 0,
+    text: "Call mom",
+  },
+  {
+    _id: 1,
+    text: "Work out",
+  },
+  {
+    _id: 2,
+    text: "Finish ReactJS talk",
+  },
+];
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState(initialData);
+
+  const handleSubmit = () => {
+    if (inputValue === "") return;
+    const newTodo = { _id: Date.now(), text: inputValue };
+    setTodos([...todos, newTodo]);
+    setInputValue("");
+  };
+
+  const handleDeleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo._id !== id);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todolist">
+        <Title myName="goldmouthdogg" />
+        <Form
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          handleSubmit={handleSubmit}
+        />
+        {todos.map((todo, index) => (
+          <Todo
+            todo={todo}
+            index={index}
+            handleDeleteTodo={handleDeleteTodo}
+            key={todo._id}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
